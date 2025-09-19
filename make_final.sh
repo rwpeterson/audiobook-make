49d328cfd4e9257f2f807b12a75637d5698da900
Bob Peterson <bob@rwp.is>
2025-09-18 21:04:37 -0500

    cover fix

--- make_final.sh
+++ make_final.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-ffmpeg -f concat -safe 0 -i filelist.txt -i cover.jpg -f ffmetadata -i chapters.txt -c:a copy -c:v mjpeg -disposition:v attached_pic -map_metadata 2 output.mp3
+ffmpeg -f concat -safe 0 -i filelist.txt -i cover.jpg -i chapters.txt \
+-map 0:a -map 1 -map_metadata 2 \
+-c:a copy -c:v mjpeg \
+-disposition:v attached_pic \
+-id3v2_version 3 \
+output.mp3
 
 echo "Wrote concatenated audio with chapter and cover to output.mp3"
